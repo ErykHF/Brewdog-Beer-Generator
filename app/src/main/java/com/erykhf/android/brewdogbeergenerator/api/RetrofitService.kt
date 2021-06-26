43b761a529146615e412b80f24b3fb0372927c4f
Eryk Ferenc <Cybersnake@hotmail.co.uk>
2021-06-26 17:46:52 +0100

    separated retrofit service

---
+++ app/src/main/java/com/erykhf/android/brewdogbeergenerator/api/RetrofitService.kt
@@ -0,0 +1,55 @@
+package com.erykhf.android.brewdogbeergenerator.api
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.erykhf.android.brewdogbeergenerator.model.BeerData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val BASE_URL = "https://api.punkapi.com/v2/beers/"
+
+
+class RetrofitService {
+
+    private val punkApiService: PunkApiService
+
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl((BASE_URL))
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+
+        punkApiService = retrofit.create(PunkApiService::class.java)
+    }
+
+
+    fun getBeerImageResponse(): LiveData<List<BeerData>> {
+        val liveDataResponse: MutableLiveData<List<BeerData>> = MutableLiveData()
+        val callPunkApi: Call<List<BeerData>> = punkApiService.loadImages()
+
+        callPunkApi.enqueue(object : Callback<List<BeerData>> {
+            override fun onResponse(
+                    call: Call<List<BeerData>>,
+                    response: Response<List<BeerData>>
+            ) {
+                val beerResponse: List<BeerData>? = response.body()
+
+
+
+                liveDataResponse.value = beerResponse
+
+            }
+
+            override fun onFailure(call: Call<List<BeerData>>, t: Throwable) {
+                Log.e("MainActivity", "Failed to get search results", t)
+            }
+
+        })
+        return liveDataResponse
+    }
+}
--- app/src/main/java/com/erykhf/android/brewdogbeergenerator/ui/main/MainFragment.kt
+++ app/src/main/java/com/erykhf/android/brewdogbeergenerator/ui/main/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.erykhf.android.brewdogbeergenerator.ui.main
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -9,10 +11,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import com.erykhf.android.brewdogbeergenerator.GlideImageLoader
 import com.erykhf.android.brewdogbeergenerator.ImageLoader
 import com.erykhf.android.brewdogbeergenerator.R
 import com.erykhf.android.brewdogbeergenerator.api.PunkApiService
+import com.erykhf.android.brewdogbeergenerator.api.RetrofitService
 import com.erykhf.android.brewdogbeergenerator.model.BeerData
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,20 +27,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 private const val BASE_URL = "https://api.punkapi.com/v2/beers/"
+private const val TAG = "MainActivity"
 
 class MainFragment : Fragment() {
-
-
-    private val retrofit by lazy {
-        Retrofit.Builder()
-                .baseUrl((BASE_URL))
-                .addConverterFactory(MoshiConverterFactory.create())
-                .build()
-    }
-
-    private val punkApiService by lazy {
-        retrofit.create(PunkApiService::class.java)
-    }
 
     private var beerName: TextView? = view?.findViewById(R.id.beer_name)
     private var profileImageView: ImageView? = view?.findViewById(R.id.main_profile_image)
@@ -46,50 +41,70 @@ class MainFragment : Fragment() {
     private val imageLoader: ImageLoader by lazy {
         GlideImageLoader(requireActivity())
     }
-
-
-    private fun getBeerImageResponse() {
-        val call = punkApiService.loadImages()
-        call.enqueue(object : Callback<List<BeerData>> {
-            override fun onResponse(
-                    call: Call<List<BeerData>>,
-                    response: Response<List<BeerData>>
-            ) {
-                if (response.isSuccessful) {
-
-                    Log.d("MainActivity", "onResponse: ${response.body()}")
-
-                    val results = response.body()
-
-                    beerName?.text = results?.firstOrNull()?.name ?: "Unknown"
-                    descriptionResponse?.text = results?.firstOrNull()?.description
-                            ?: "No Description"
-                    firstBrewed?.text = ("First brewed: ${results?.firstOrNull()?.first_brewed}" ) ?: "No Data"
-                    tagLine?.text = results?.firstOrNull()?.tagline ?: "No tags"
-
-
-                    val firstImageUrl = results?.firstOrNull()?.image_url ?: ""
-                    if (firstImageUrl.isNotBlank()) {
-                        if (profileImageView != null) {
-                            imageLoader.loadImage(
-                                    firstImageUrl,
-                                    profileImageView!!
-                            )
-                        }
-                    } else {
-                        Log.d("MainActivity", "Missing image URL")
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<BeerData>>, t: Throwable) {
-                Log.e("MainActivity", "Failed to get search results", t)
-            }
-
-        })
-    }
+//
+//
+//    private fun getBeerImageResponse() {
+//        val call = punkApiService.loadImages()
+//        call.enqueue(object : Callback<List<BeerData>> {
+//            override fun onResponse(
+//                    call: Call<List<BeerData>>,
+//                    response: Response<List<BeerData>>
+//            ) {
+//                if (response.isSuccessful) {
+//
+//                    Log.d("MainActivity", "onResponse: ${response.body()}")
+//
+//                    val results = response.body()
+//
+//                    beerName?.text = results?.firstOrNull()?.name ?: "Unknown"
+//                    descriptionResponse?.text = results?.firstOrNull()?.description
+//                            ?: "No Description"
+//                    firstBrewed?.text = ("First brewed: ${results?.firstOrNull()?.first_brewed}" ) ?: "No Data"
+//                    tagLine?.text = results?.firstOrNull()?.tagline ?: "No tags"
+//
+//
+//                    val firstImageUrl = results?.firstOrNull()?.image_url ?: ""
+//                    if (firstImageUrl.isNotBlank()) {
+//                        if (profileImageView != null) {
+//                            imageLoader.loadImage(
+//                                    firstImageUrl,
+//                                    profileImageView!!
+//                            )
+//                        }
+//                    } else {
+//                        Log.d("MainActivity", "Missing image URL")
+//                    }
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<BeerData>>, t: Throwable) {
+//                Log.e("MainActivity", "Failed to get search results", t)
+//            }
+//
+//        })
+//    }
 
     private lateinit var viewModel: MainViewModel
+
+    private fun getBeerResponse() {
+        val punkApiLiveData: LiveData<List<BeerData>> = RetrofitService().getBeerImageResponse()
+        punkApiLiveData.observe(requireActivity(), Observer { beerResponse ->
+            Log.d(TAG, "onCreateView: $beerResponse")
+
+            val noImagePlaceHolder = "https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png"
+            val imageUrl = beerResponse?.firstOrNull()?.image_url ?: noImagePlaceHolder
+
+            if (profileImageView != null) {
+                imageLoader.loadImage(imageUrl, profileImageView!!)
+            }
+            beerName?.text = beerResponse?.firstOrNull()?.name ?: "Unknown"
+            descriptionResponse?.text = beerResponse?.firstOrNull()?.description
+                    ?: "No Description"
+            firstBrewed?.text = ("First brewed: ${beerResponse?.firstOrNull()?.first_brewed}")
+                    ?: "No Data"
+            tagLine?.text = beerResponse?.firstOrNull()?.tagline ?: "No tags"
+        })
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -103,10 +118,9 @@ class MainFragment : Fragment() {
         firstBrewed = view.findViewById(R.id.first_brewed)
 
         profileImageView?.setOnClickListener {
-            getBeerImageResponse()
+            getBeerResponse()
         }
-        getBeerImageResponse()
-
+        getBeerResponse()
 
         return view
     }
